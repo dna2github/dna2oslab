bbed5939a7e4fd352ea0297baec5b46d2c47fa65
Seven Lju <dna2gle@gmail.com>
2016-11-27 01:49:42 +0800

    [android build] fix node v4 cmd history

--- android/build/node-v4.4.4.sh
+++ android/build/node-v4.4.4.sh
@@ -1,17 +1,5 @@
 #/bin/bash
 
-echo "===================================================================="
-echo "Notice: if it throws error and report something wrong"
-echo "about '.node_repl_history' to run ./node, please open"
-echo "'lib/internal/repl.js' under source code directory,"
-echo "find this line:"
-echo "   historyPath = path.join(os.homedir(), '.node_repl_history');"
-echo " and fix the path like:"
-echo "   historyPath = '/data/local/tmp/.node_repl_history';"
-echo " then rebuild nodejs."
-echo "===================================================================="
-sleep 2
-
 set -xe
 
 MEDIR=$(cd `dirname $0`; pwd)
@@ -30,6 +18,7 @@ mkdir -p dist out
 
 cp $NDKDIR/sources/cxx-stl/gnu-libstdc++/${GCC_VERSION}/libs/armeabi/lib* out/
 cp $ANDROID/lib/* out/
+sed -i "s|historyPath = path.join.*|historyPath = '/data/local/tmp/.node_repl_history';|" $MEDIR/../$ME/lib/internal/repl.js
 sed -i "s/uv__getiovmax()/1024/" $MEDIR/../$ME/deps/uv/src/unix/fs.c
 sed -i "s/uv__getiovmax()/1024/" $MEDIR/../$ME/deps/uv/src/unix/stream.c
 sed -i "s/path.join(os.homedir(), '.node_repl_history');/'\/data\/local\/tmp\/.node_repl_history';/" $MEDIR/../$ME/lib/internal/repl.js
